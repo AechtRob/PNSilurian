25cb57750af6e59875a22af0beaed3016aad94b5
AechtRob <aecht_rob@outlook.com>
2022-09-29 23:57:02 +0100

    Initial commit

---
+++ src/main/java/net/pnsilurian/IProxyPNSilurianMod.java
@@ -0,0 +1,17 @@
+package net.pnsilurian;
+
+
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLServerStartingEvent;
+
+public interface IProxyPNSilurianMod {
+    void preInit(FMLPreInitializationEvent event);
+
+    void init(FMLInitializationEvent event);
+
+    void postInit(FMLPostInitializationEvent event);
+
+    void serverLoad(FMLServerStartingEvent event);
+}
