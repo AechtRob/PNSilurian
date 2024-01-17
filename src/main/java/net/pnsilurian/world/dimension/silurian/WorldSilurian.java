f7d8ab50ef4a09ec3234ce9812cf94cea6828cd2
AechtRob <aecht_rob@outlook.com>
2024-01-17 14:09:40 +0000

    Portals

--- src/main/java/net/pnsilurian/world/dimension/silurian/WorldSilurian.java
+++ src/main/java/net/pnsilurian/world/dimension/silurian/WorldSilurian.java
@@ -1022,7 +1022,7 @@ public class WorldSilurian extends ElementsLepidodendronMod.ModElement {
             entityIn.setWorld(toWorldIn);
         }
 
-        private TeleporterDimensionMod getTeleporterForDimension(Entity entity, BlockPos pos, int dimid) {
+        public static TeleporterDimensionMod getTeleporterForDimension(Entity entity, BlockPos pos, int dimid) {
             BlockPattern.PatternHelper bph = portal.createPatternHelper(entity.world, new BlockPos(pos));
             double d0 = bph.getForwards().getAxis() == EnumFacing.Axis.X
                     ? (double) bph.getFrontTopLeft().getZ()
